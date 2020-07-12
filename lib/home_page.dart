0ead624f409ca9e5967029c5f3b62f9d6ffa93e0
yogev sasssoni <yogevsas@gmail.com>
2020-07-12 21:54:33 +0300

    tab controller

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,12 @@ class HomePage extends StatelessWidget {
   final BaseAuth auth;
   final VoidCallback onSignOut;
 
+  final tab = new TabBar(tabs: <Tab>[
+    new Tab(icon: new Icon(Icons.arrow_forward)),
+    new Tab(icon: new Icon(Icons.arrow_downward)),
+    new Tab(icon: new Icon(Icons.arrow_back)),
+  ]);
+
   @override
   Widget build(BuildContext context) {
     void _signOut() async {
@@ -19,82 +25,106 @@ class HomePage extends StatelessWidget {
       }
     }
 
-    return new Scaffold(
-      appBar: new AppBar(
-        actions: <Widget>[
-          new FlatButton(
-              onPressed: _signOut,
-              child: new Text('Logout',
-                  style: new TextStyle(fontSize: 17.0, color: Colors.white)))
-        ],
-      ),
-      body: Column(
-        children: <Widget>[
-          Flexible(
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: new AppBar(
+            actions: <Widget>[
+              new FlatButton(
+                  onPressed: _signOut,
+                  child: new Text('Logout',
+                      style:
+                      new TextStyle(fontSize: 17.0, color: Colors.white))),
+            ],
+            bottom: tab,
+          ),
+          body: Column(children: <Widget>[
+            SizedBox(
+              height: 300,
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      child: TabBarView(
+                        children: <Widget>[
+                          Container(
+                              child: ListView(
+                                shrinkWrap: true,
+                                children: <Widget>[
+                                  ListTile(
+                                    title: Text('Event1'),
+                                    onTap: () {
+                                      // Update the state of the app.
+                                      // ...
+                                    },
+                                  ),
+                                  ListTile(
+                                    title: Text('Event2'),
+                                    onTap: () {
+                                      // Update the state of the app.
+                                      // ...
+                                    },
+                                  ),
+                                ],
+                              ),
+                          ),
+                          Container(
+                            height: 200.0,
+                            color: Colors.green,
+                          ),
+                          Container(
+                            height: 200.0,
+                            color: Colors.red,
+                          ),
+                        ]
+                      ),
+                    ),
+                  ],
+                ))
+          ]),
+          drawer: Drawer(
             child: ListView(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
               children: <Widget>[
+                DrawerHeader(
+                  child: Text('Settings'),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                ),
                 ListTile(
-                  title: Text('Event1'),
+                  title: Text('ROFL'),
                   onTap: () {
                     // Update the state of the app.
                     // ...
                   },
                 ),
                 ListTile(
-                  title: Text('Event2'),
+                  title: Text('DAB'),
                   onTap: () {
                     // Update the state of the app.
                     // ...
+                  },
+                ),
+                ListTile(
+                  title: Text('exit'),
+                  onTap: () {
+                    Navigator.pop(context);
                   },
                 ),
               ],
             ),
           ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text('Settings'),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-            ),
-            ListTile(
-              title: Text('ROFL'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: Text('DAB'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: Text('exit'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => AddEvent()));
+            },
+            child: Icon(Icons.add),
+            backgroundColor: Colors.blue,
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => AddEvent()));
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.blue,
       ),
     );
   }
