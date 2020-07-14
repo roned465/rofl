f10ec600cfcbf9d649a83850993f6d4e5d28ad79
ShlomiLevush <shlomilevush@gmail.com>
2020-07-14 09:15:41 +0300

    Loading screen

--- lib/root_page.dart
+++ lib/root_page.dart
@@ -13,13 +13,14 @@ class RootPage extends StatefulWidget {
 }
 
 enum AuthStatus {
+  loading,
   notSignedIn,
   signedIn,
 }
 
 class _RootPageState extends State<RootPage> {
 
-  AuthStatus authStatus = AuthStatus.notSignedIn;
+  AuthStatus authStatus = AuthStatus.loading;
 
   initState() {
     super.initState();
@@ -39,6 +40,10 @@ class _RootPageState extends State<RootPage> {
   @override
   Widget build(BuildContext context) {
     switch (authStatus) {
+      case AuthStatus.loading:
+        return new Scaffold(
+
+        );
       case AuthStatus.notSignedIn:
         return new LoginPage(
           title: 'Flutter Login',
